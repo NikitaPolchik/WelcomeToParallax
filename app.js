3b034ca157850280ba29e468001bcac9f757f72f
Your Name <you@example.com>
2022-09-06 02:19:56 +0300

    New commit  On branch master  Initial commit Changes to be committed: 	new file:   app.js 	new file:   index.html 	new file:   styles.css

---
+++ app.js
@@ -0,0 +1,16 @@
+// ScrollMagic function
+
+function splitScroll() {
+  const controller = new ScrollMagic.Controller();
+
+  new ScrollMagic.Scene({
+    duration: "200%",
+    triggerElement: ".about-title",
+    triggerHook: 0,
+  })
+    .setPin(".about-title h2")
+    .addIndicators()
+    .addTo(controller);
+}
+
+splitScroll();
